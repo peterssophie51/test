9ac9b22c144fbb71d6ac8f7f875988d9cf2c05aa
Sophie Peters <peterssophie51@gmail.com>
2024-06-25 10:26:19 +1200

    streaming a component using suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,8 @@ import {
   fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons'
  
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
@@ -32,7 +34,9 @@ export default async function Page() {
         /> 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
