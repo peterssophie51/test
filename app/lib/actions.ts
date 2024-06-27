0ea1d0590c3a345891c5afe72ad0c79e0c6d084e
Sophie Peters <peterssophie51@gmail.com>
2024-06-27 14:25:52 +1200

    error handling and such

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,7 +65,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
 export async function deleteInvoice(id: string) {
-    try {
+  throw new Error('Failed to Delete Invoice')
+  try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices')
       return { message: 'Deleted Invoice.' };
